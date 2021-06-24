e3435056793546187745b7e17f3e736f33832da6
VolTorian <voltorian.social@gmail.com>
2021-06-23 18:39:16 -0700

    in game timer
    added timers for time spent moving, time standing still, and total time. total time may sometimes be off by .01 seconds due to truncating. it was either that or have the timer sometimes display 0 seconds and 100 centiseconds instead of 1 second and 0 centiseconds. i can't be bothered to spend more time on this rn

--- TimePuzzle/Assets/Scripts/LevelManager.cs
+++ TimePuzzle/Assets/Scripts/LevelManager.cs
@@ -2,33 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
     public GameObject spawnpoint;
-    private PlayerController player;
+    PlayerController player;
     public GameObject deathParticle;
     public GameObject respawnParticle;
     public float respawnDelay;
     public string level;
-    
+
+    public TextMeshProUGUI movingTimer;
+    public TextMeshProUGUI stillTimer;
+    public TextMeshProUGUI overallTimer;
+    float movingTotalTime;
+    float stillTotalTime;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
-        //player.body
+        movingTotalTime = 0;
+        stillTotalTime = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.GetIsMoving())
+        {
+            movingTotalTime += Time.deltaTime;
+            movingTimer.text = "Moving:\n    " + FormatTime(movingTotalTime);
+        }
+        else
+        {
+            stillTotalTime += Time.deltaTime;
+            stillTimer.text = "Stationary:\n    " + FormatTime(stillTotalTime);
+        }
+        overallTimer.text = "Total:\n    " + FormatTime(stillTotalTime + movingTotalTime);
     }
 
     public void RespawnPlayer()
     {
         StartCoroutine("RespawnPlayerCo");
+    }
+
+    string FormatTime(float totalTime)
+    {
+        int minutes = ((int)totalTime) / 60;
+        int seconds = ((int)totalTime) % 60;
+        float centiseconds = totalTime * 100;
+        centiseconds = centiseconds % 100;
+        //int centiseconds = Mathf.RoundToInt((totalTime * 100) % 100);
+        return string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, (int)centiseconds);
     }
 
     public IEnumerator RespawnPlayerCo()
